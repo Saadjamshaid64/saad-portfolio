ca3f63402345783462bef0963eba85adae7841d4
Saad Jamshaid <saadjamshaid64@gmail.com>
2026-05-27 21:53:54 +0500

    Added Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import ReactGA from "react-ga4";
 
 const queryClient = new QueryClient();
+
+ReactGA.initialize("G-S481QKFSDP");
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
